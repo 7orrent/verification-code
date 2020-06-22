94c184b62b1e2a1be39ad743ef07ca97963e78b5
7orrent <7orrent.sin@gmail.com>
2020-06-22 10:58:19 +0100

    Unbind Added.

--- commands/info/check.js
+++ commands/info/check.js
@@ -11,7 +11,7 @@ module.exports = {
         let user = message.member.user;
         let member = message.guild.member(user);
 
-        if (!member.roles.cache.find(r => r.name === "Outsiders")) return message.channel.send("\`You're already verified!\`").then(msg => {msg.delete({ timeout: 5000 })}).catch(console.error);
+        if (!member.roles.cache.find(r => r.name === "Followers")) return message.channel.send("\`Invalid Permissions.\`").then(msg => {msg.delete({ timeout: 5000 })}).catch(console.error);
 
         if (args.length === 0) return message.channel.send("\`Please provide a valid code.\`").then(msg => {msg.delete({ timeout: 5000 })}).catch(console.error);
 
